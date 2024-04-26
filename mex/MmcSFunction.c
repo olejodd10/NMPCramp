159436630fcc8ef6a09aaea311024ec016355f20
Ole Johan Walderhaug <ojwalder@stud.ntnu.no>
2024-04-26 13:51:52 +0200

    Use RK4 with 350 us Ts1

--- mex/MmcSFunction.c
+++ mex/MmcSFunction.c
@@ -12,6 +12,7 @@
 #include "SdqpLmpcMmc.h"
 
 #define MMC_DELAY 6 // Delay for Vf and Iv_ref
+#define TS1_FACTOR 5
 
 // N_X and N_U are imported from MmcModel
 #define V_SIGMA_TRAJ_0 300.0
@@ -42,7 +43,7 @@ void mmc_s_function_start(int N, real_t R, real_t Rc, real_t L, real_t Lc, real_
 
     m_omega = 2.0*M_PI*freq*Ts;
 
-    mmc_model_get_fe_init(R, Rc, L, Lc, C, Ts, n_sm, (size_t)N, CAST_3D_VLA(m_A, N_X, N_X), CAST_3D_VLA(m_B, N_X, N_U), CAST_2D_VLA(m_d, N_X));    
+    mmc_model_get_rk4_init(R, Rc, L, Lc, C, Ts, TS1_FACTOR*Ts, n_sm, (size_t)N);    
     sdqp_lmpc_mmc_init(N_X, N_U, (size_t)N, q1, q2, x_min, x_max, n_sm, insertion_index_deviation_allowance, u_min, u_max);
 
     // Initialize trajectories
@@ -76,13 +77,15 @@ int mmc_s_function(int N, real_t phase_Vf, real_t phase_Iv, real_t amp_Vf, real_
     memcpy(m_x_traj, x0, sizeof(real_t)*N_X);
 
     // Predict Vf and Iv_ref
+    real_t phase = m_omega*MMC_DELAY;
     for (int i = 0; i < N; ++i) {
-        m_Vf[i] = amp_Vf*sin(m_omega*((real_t)i + MMC_DELAY) + phase_Vf);
-        m_Iv_ref[i] = amp_Iv*sin(m_omega*((real_t)i + MMC_DELAY) + phase_Iv);
+        m_Vf[i] = amp_Vf*sin(phase + phase_Vf);
+        m_Iv_ref[i] = amp_Iv*sin(phase + phase_Iv);
+        phase += (i == 0 ? m_omega : (real_t)TS1_FACTOR*m_omega);
     }
 
     // Get new model
-    mmc_model_get_fe((size_t)N, CAST_CONST_2D_VLA(m_x_traj, N_X), CAST_CONST_2D_VLA(m_u_traj, N_U), m_Vf, Vdc, CAST_3D_VLA(m_A, N_X, N_X), CAST_3D_VLA(m_B, N_X, N_U), CAST_2D_VLA(m_d, N_X));
+    mmc_model_get_rk4((size_t)N, CAST_CONST_2D_VLA(m_x_traj, N_X), CAST_CONST_2D_VLA(m_u_traj, N_U), m_Vf, Vdc, CAST_3D_VLA(m_A, N_X, N_X), CAST_3D_VLA(m_B, N_X, N_U), CAST_2D_VLA(m_d, N_X));
 
     // Solve
     int err = sdqp_lmpc_mmc_solve(N_X, N_U, (size_t)N, m_Iv_ref, Icir_ref, CAST_CONST_3D_VLA(m_A, N_X, N_X), CAST_CONST_3D_VLA(m_B, N_X, N_U), CAST_CONST_2D_VLA(m_d, N_X), x0, x, u);
